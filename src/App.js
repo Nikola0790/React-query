4a8da9e98bfc8bbfc12cd078322e8f11f91de933
Nikola0790 <nikola.drcalic@gmail.com>
2022-06-24 12:28:03 +0200

    Made nav menu with Router and QueryClientProvider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import {QueryClient, QueryClientProvider} from "react-query"
+import { ReactQueryDevtools } from 'react-query/devtools'
+import './App.css'
+import { HomePage } from './components/HomePage'
+import { RQSuperHeroesPage } from './components/RQSuperHeroesPage'
+
+// Create a client
+const queryClient = new QueryClient ();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Provide the client to App
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/rq-super-heroes'  element={<RQSuperHeroesPage />} />
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default App;
