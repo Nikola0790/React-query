4aaa0f607abadcb6581f81b0b7e894dad7d334ac
Nikola0790 <nikola.drcalic@gmail.com>
2022-06-24 14:36:04 +0200

    useQuery on click

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -3,9 +3,11 @@ import { getSuperHeroes } from "../service/service";
 
 export const RQSuperHeroesPage = () => {
 
-  const {isLoading, isError, data, error} = useQuery('superHeroes', getSuperHeroes);
+  // enabled: boolean - Set this to false to disable this query from automatically running.
+  // refetch: A function to manually refetch the query.
+  const {isLoading, isError, data, error, refetch, isFetching} = useQuery('superHeroes', getSuperHeroes, { enabled: false, });
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return  <p>Loading...</p>
   }
 
@@ -18,12 +20,13 @@ export const RQSuperHeroesPage = () => {
       <h1>React Query Super Heroes Page</h1>
       <h2>Super Heroes List</h2>
       <ul>
-        {data.data.map(superHero => {
+        {data?.data.map(superHero => {
           return (
             <li key={superHero.id}>{superHero.name}</li>
           )
         })}
       </ul>
+      <button onClick={refetch}>Get Heroes</button>
     </div> 
   )
 };
