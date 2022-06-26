3733412722db124b91fab99a2356216e17d437a9
Nikola0790 <nikola.drcalic@gmail.com>
2022-06-26 16:07:57 +0200

    Added route for SingleSuperHero page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import {QueryClient, QueryClientProvider} from "react-query"
-import { ReactQueryDevtools } from 'react-query/devtools'
-import './App.css'
-import { HomePage } from './components/HomePage'
-import { RQSuperHeroesPage } from './components/RQSuperHeroesPage'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import "./App.css";
+import { HomePage } from "./components/HomePage";
+import { RQSuperHeroesPage } from "./components/RQSuperHeroesPage";
+import { SingleSuperHero } from "./components/RQSingleSuperHeroPage";
 
 // Create a client
-const queryClient = new QueryClient ();
+const queryClient = new QueryClient();
 
 function App() {
   return (
@@ -17,22 +18,26 @@ function App() {
           <nav>
             <ul>
               <li>
-                <Link to='/'>Home</Link>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to='/rq-super-heroes'>Super Heroes</Link>
+                <Link to="/rq-super-heroes">Super Heroes</Link>
               </li>
             </ul>
           </nav>
           <Routes>
-            <Route path='/rq-super-heroes'  element={<RQSuperHeroesPage />} />
-            <Route path='/' element={<HomePage />} />
+            <Route
+              path="/rq-super-hero/:heroId"
+              element={<SingleSuperHero />}
+            />
+            <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+            <Route path="/" element={<HomePage />} />
           </Routes>
         </div>
       </Router>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  )
+  );
 }
 
 export default App;
