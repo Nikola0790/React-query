4c1ed452d3de363e44a220ad93b65657b235f7a2
Nikola0790 <nikola.drcalic@gmail.com>
2022-06-26 16:12:04 +0200

    Made request getSuperHero

--- src/service/service.js
+++ src/service/service.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
 export const getSuperHeroes = async () => {
-    const results = await axios.get('http://localhost:4000/superheroes');
-    return results;
+  const results = await axios.get("http://localhost:4000/superheroes");
+  return results;
+};
+
+export const getSuperHero = async ({ queryKey }) => {
+  const id = queryKey[1];
+  const result = await axios.get(`http://localhost:4000/superheroes/${id}`);
+  return result;
 };
