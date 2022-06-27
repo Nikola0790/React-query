56a3c3bd289a8561475655b46b660e1f6a13f77d
Nikola0790 <nikola.drcalic@gmail.com>
2022-06-27 16:40:00 +0200

    Made request getCars and getSuperHero_2

--- src/service/service.js
+++ src/service/service.js
@@ -10,3 +10,14 @@ export const getSuperHero = async ({ queryKey }) => {
   const result = await axios.get(`http://localhost:4000/superheroes/${id}`);
   return result;
 };
+
+// For dynamic parallel queries
+export const getSuperHero_2 = async (id) => {
+  const result = await axios.get(`http://localhost:4000/superheroes/${id}`);
+  return result;
+};
+
+export const getCars = async () => {
+  const results = await axios.get("http://localhost:4000/cars");
+  return results;
+};
