c2bb9891c706b79ae6150e94715b967074329cf0
Nikola0790 <nikola.drcalic@gmail.com>
2022-06-27 16:41:29 +0200

    Added routes and links for parallel-queries and dynamic-parallel-queries

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import "./App.css";
 import { HomePage } from "./components/HomePage";
 import { RQSuperHeroesPage } from "./components/RQSuperHeroesPage";
 import { SingleSuperHero } from "./components/RQSingleSuperHeroPage";
+import { ParallelQueries } from "./components/ParallelQueries";
+import { DynamicParallelQueries } from "./components/DynamicParallelQueries";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -23,9 +25,22 @@ function App() {
               <li>
                 <Link to="/rq-super-heroes">Super Heroes</Link>
               </li>
+              <li>
+                <Link to="/parallel-queries">Parallel Queries</Link>
+              </li>
+              <li>
+                <Link to="/dynamic-parallel-queries">
+                  Dynamic Parallel Queries
+                </Link>
+              </li>
             </ul>
           </nav>
           <Routes>
+            <Route
+              path="/dynamic-parallel-queries"
+              element={<DynamicParallelQueries heroIds={[1, 3]} />}
+            />
+            <Route path="/parallel-queries" element={<ParallelQueries />} />
             <Route
               path="/rq-super-hero/:heroId"
               element={<SingleSuperHero />}
